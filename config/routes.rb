3e11c814625e3d463e6d1b0834f7d4b7dc2ad4fa
sho-kasama <sho-kasama@trunk.fm>
2019-03-07 19:42:13 +0900

    add :route

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
+
+
+
+
+# static_pages/home
+# このルールは↑のURLに対するリクエストをStaticPagesコントローラのhomeアクションと結びつけています。
+# 今回はgetと書かれているため、GETリクエストを受け取った時に対応するアクションを結びつけています。
