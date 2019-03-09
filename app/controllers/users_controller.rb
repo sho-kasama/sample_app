4948017df787dbef4829046b416c9b36f3bb9b2f
sho-kasama <sho-kasama@trunk.fm>
2019-03-09 13:20:54 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
-end
 
 
-# このコードはとてもよくまとまっているコード
-# class User < ApplicationRecord　という構文で、Userクラスは ApplicationRecordを継承するので
-# Userモデルは自動的にActiveRecord::Baseクラスのすべての機能を持つことになります 
-# とはいえActiveRecord::Base に含まれるメソッドがわからない
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else 
+      render 'new'
+    end
+  end
+
+
+
+  private 
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
