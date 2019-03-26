c813ca5332d62a99ab60611343c8aea4d214e9f1
sho-kasama <sho-kasama@trunk.fm>
2019-03-27 02:59:34 +0900

    ref #19 add: メールの有効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'application#hello'
   root 'static_pages#home'
